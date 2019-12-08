083e7e9a499b98c83a3ec4889e9c8cd4c1fac4fe
habitat <chenkang@ewell.cc>
2019-12-08 12:04:34 +0800

    添加externals

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Button from './Button'
 
 console.log(1);
 console.log(React);
 console.log(ReactDOM);
+
+ReactDOM.render(<Button />, document.body)
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
     entry: {
         ckui: './lib/index.tsx'
     },
+    resolve: {
+        extensions: [ '.ts', '.tsx', '.js', 'jsx' ]
+    },
     output: {
         path: path.resolve(__dirname, 'dist/lib'),
         library: 'ckui',
@@ -19,6 +22,20 @@ module.exports = {
             }
         ]
 
+    },
+    externals: {
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+            root: React
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDOM'
+        }
     },
     plugins: [
         new HtmlWebpackPlugin({
