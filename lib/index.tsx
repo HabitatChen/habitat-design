9702945c2acfe3edbc91e5a670b0ba89fca1c256
habitat <chenkang@ewell.cc>
2020-01-01 22:30:26 +0800

    发布包

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,24 +1,3 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
 // import Button from './Button'
 // @ts-ignore
-import Icon from './icon/Icon'
-
-const fn: React.MouseEventHandler = (e) => {
-    console.log(e)
-    console.log(e.target)
-    // 断言
-    console.log((e.target as SVGUseElement).href )
-}
-
-ReactDOM.render(
-    <div>
-        <Icon
-            name='wechat'
-            onClick={ fn }
-            onMouseEnter={ () => {
-                console.log('enter')
-            }}
-        />
-    </div>,
-    document.querySelector('#root'))
+export { default as  Icon } from './icon/Icon'
